e700b02b15aad2175cf8cc199f7d825e6ac73b15
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-02 00:19:28 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,9 @@
 def using_while
   levitation_force = 6
 
-  loop do
-    levitation_force += 1
-    if "#{levitation_force}" <= 10
-      puts "Wingardium Leviosa"
-      break
+loop do
+  levitation_force += 1
+  if "#{levitation_force}" <= 10
+    puts "Wingardium Leviosa"
+    break
   end
-end
